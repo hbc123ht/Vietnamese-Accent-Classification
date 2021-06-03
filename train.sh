f485aa99af3816ea3a7e88f694e0336e8732c387
hbc123ht <hbc123ht@gamil.com>
2021-06-03 13:17:21 +0000

    update

--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
-python3 train.py --SAVE_CHECKPOINT_FREQUENCY 1 \
-                 --NUM_EPOCH 10 \
-                 --CHECKPOINT_DIR checkpoint \
-                 --DATA_DIR wavv \
-                 --NUM_EPOCH 1000
+python3 train.py --SAVE_CHECKPOINT_FREQUENCY 200 \
+                 --CHECKPOINT_DIR checkpointt \
+                 --DATA_DIR wav \
+                 --NUM_EPOCH 100000 \
+                 --STEPS_PER_EPOCH 100 \
+                 --BATCH_SIZE 30 \
+                 --LOAD_CHECKPOINT_DIR checkpoint/model.106.h5
 
 
