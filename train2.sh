5dba6577a3c934cc9b43ba53f6eb32e62555dd9b
hbc123ht <hbc123ht@gmail.com>
2021-06-04 16:36:19 +0000

    update

--- train2.sh
+++ train2.sh
@@ -1,9 +1,10 @@
-python3 train2.py --SAVE_CHECKPOINT_FREQUENCY 1000 \
-                 --CHECKPOINT_DIR checkpointt \
+python3 train2.py --SAVE_CHECKPOINT_FREQUENCY 200 \
+                 --CHECKPOINT_DIR checkpoint \
                  --DATA_DIR wav \
                  --NUM_EPOCH 1000000 \
                  --STEPS_PER_EPOCH 1000 \
                  --COL_SIZE 15 \
-                 --LOG log1
+                 --LOG log1 \
+                 --LR 0.07
 
 
