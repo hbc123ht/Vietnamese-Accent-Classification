1793aa479957e35111a261004c5689b1def1a5b6
hbc123ht <hbc123ht@gmail.com>
2021-06-05 06:17:48 +0000

    change lr

--- train2.sh
+++ train2.sh
@@ -3,8 +3,8 @@ python3 train2.py --SAVE_CHECKPOINT_FREQUENCY 200 \
                  --DATA_DIR wav \
                  --NUM_EPOCH 1000000 \
                  --STEPS_PER_EPOCH 1000 \
-                 --COL_SIZE 50 \
+                 --COL_SIZE 30 \
                  --LOG log1 \
-                 --LR 0.07
+                 --LR 0.05
 
 
