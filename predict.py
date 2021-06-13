7fdd538e5bafdf7864688ba8101d8f30dbaa142a
hbc123ht <hbc123ht@gmail.com>
2021-06-13 15:24:27 +0700

    code refactor

--- predict.py
+++ predict.py
@@ -4,6 +4,8 @@ import numpy as np
 import tensorflow as tf
 import multiprocessing
 import argparse
+from dynaconf import settings
+settings.load_file(path="config.py")
 
 from utils import (get_wav, to_mfcc, load_categories,
                     normalize_mfcc, make_segments, 
@@ -41,10 +43,10 @@ if __name__ == '__main__':
     args = parser()
 
     #labels
-    categories = load_categories(args.CATEGORIES_DIR)
+    categories = load_categories(os.path.join(settings.DATA_DIR, 'categories/labels.json'))
     
     #load data
-    X, y = load_data(args.DATA_DIR, categories = categories)
+    X, y = load_data(os.path.join(settings.DATA_DIR,'data'), categories = categories)
 
 
     # Get resampled wav files using multiprocessing
@@ -62,19 +64,19 @@ if __name__ == '__main__':
     X = p_map(normalize_mfcc,X)
 
     # # get the input shape 
-    input_shape = get_input_shape(X, args.COL_SIZE)
+    input_shape = get_input_shape(X, settings.COL_SIZE)
     
     # # initiate model
-    model = Model(input_shape = input_shape, num_classes = len(categories), lr = args.LR)
+    model = Model(input_shape = input_shape, num_classes = len(categories), lr = settings.LR)
 
     #load the weights                
     try:
-        model.load_weights(args.LOAD_CHECKPOINT_DIR)
+        model.load_weights(settings.LOAD_CHECKPOINT_DIR)
     except:
         logging.error('Unable to load checkpoints')
 
     for X_,y_ in zip(X, y):
-        X_, y_ = segment_one(X_ ,y_, COL_SIZE = args.COL_SIZE)
+        X_, y_ = segment_one(X_ ,y_, COL_SIZE = settings.COL_SIZE)
         prediction = model.predict(X_)
         prediction = np.argmax(np.sum(prediction,axis = 0))
         prediction = [key for key, value in categories.items() if value == prediction]
