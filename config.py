ca7a341748d4a2b004239d57d255e9c118dc5a70
hbc123ht <hbc123ht@gmail.com>
2021-06-15 09:34:45 +0000

    finetune config

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 DEBUG = True
 
 # You could get it the length of a segment, 1.5 s by default 
-COL_SIZE = 45 
+COL_SIZE = 30
 
 # Frequency for saving checkpoint
-SAVE_CHECKPOINT_FREQUENCY = 50 
+SAVE_CHECKPOINT_FREQUENCY = 30
 
  # Total number of epochs
 NUM_EPOCH = 100
 
  # Directory of data
-DATA_DIR = 'wavv'
+DATA_DIR = 'wav'
 
  #Dir of checkpoint
 CHECKPOINT_DIR = 'checkpoint'
@@ -19,13 +19,13 @@ CHECKPOINT_DIR = 'checkpoint'
 LOG = 'logs' 
 
 #Batch size
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 
 # Num steps per epoch
 STEPS_PER_EPOCH = 128
 
 #Dir of checkpoint if u use a pretrained model, None by default
-LOAD_CHECKPOINT_DIR = None#'checkpoint/model.149.h5'
+LOAD_CHECKPOINT_DIR = None#'pretrained/model.149.h5'
 
 #Initial learning rate
 LR = 0.05
