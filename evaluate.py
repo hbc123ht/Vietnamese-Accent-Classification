979e6e9f48160cc580a204f74c785e59b4c382fc
hbc123ht <hbc123ht@gmail.com>
2021-07-02 02:13:40 +0700

    code refactor

--- evaluate.py
+++ evaluate.py
@@ -1,74 +1,56 @@
 import os
 from p_tqdm import p_map
-
 import numpy as np
 import tensorflow as tf
 import multiprocessing
 from dynaconf import settings
 settings.load_file(path="config.py")
 
-from utils import (get_wav, to_mfcc, load_categories, to_mel, add_dim, 
+from utils import (get_wav, to_mfcc, load_categories, to_mel,
                     normalize_mfcc, make_segments, 
-                    segment_one, load_data, get_input_shape)
+                    segment_one, load_data, get_input_shape, make_segment, add_dim)
                             
-from model import ResNet18
+from model import Model
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.ERROR)
 
-# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 if __name__ == '__main__':
-    
+
     #labels
     categories = load_categories(os.path.join(settings.DATA_DIR, 'categories/labels.json'))
     
     #load data
-    X, y = load_data(os.path.join(settings.DATA_DIR,'data'), categories = categories)
+    X, y = load_data(os.path.join(settings.EVALUATE_DIR,'data'), categories = categories)
 
-    #get input shape
-    tmp = get_wav(X[0])
-    
-    tmp, _ = segment_one(tmp ,y, COL_SIZE = settings.COL_SIZE, OVERLAP_SIZE = settings.OVERLAP_SIZE)
 
-    tmp = to_mfcc(tmp[0])
-
-    tmp = add_dim(tmp)
-
-    input_shape = get_input_shape(tmp)
-
-    # Get resampled wav files using multiprocessing
-    logging.info('Loading wav files....')
-
-    X = p_map(get_wav, X)
-
-    # # initiate model
-    model = ResNet18(len(categories))
-    model.build(input_shape = input_shape)
-    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              optimizer=tf.keras.optimizers.Adadelta(lr=settings.LR),
-              metrics=['accuracy'])
-
-    #load the weights                
-    try:
-        model.load_weights(settings.LOAD_CHECKPOINT_DIR)
-    except:
-        logging.error('Unable to load checkpoints')
-
-    #Evaluates
-    acc = 0.0
-
-    for X_,y_ in zip(X, y):
-
-        X_, y_ = segment_one(X_ ,y_, COL_SIZE = settings.COL_SIZE, OVERLAP_SIZE = settings.OVERLAP_SIZE)
+    model = tf.keras.models.load_model(settings.LOAD_MODEL_DIR)
+    sum = 0.
+    acc = 0.
+    for X_, y_ in zip(X,y):
+        print(y_)
+        X_ = get_wav(X_)
+        X_ = make_segment(X_, COL_SIZE = settings.COL_SIZE, OVERLAP_SIZE = settings.OVERLAP_SIZE)
         X_ = p_map(to_mfcc, X_)
         X_ = p_map(normalize_mfcc,X_)
         X_ = p_map(add_dim, X_)
+        prediction = model.predict(np.array(X_))
+        prediction = np.argmax(prediction, axis = 1)
+        prediction = np.bincount(prediction)
+        prediction = np.argmax(prediction)
         try:
-            score = model.evaluate(np.array(X_), np.array(y_))
-            acc += (score[1] >= 0.17)
+            prediction = model.predict(np.array(X_))
+            prediction = np.argmax(prediction, axis = 1)
+            prediction = np.bincount(prediction)
+            prediction = np.argmax(prediction)
+            print(prediction)
+            if prediction == y_:
+                acc = acc + 1
         except:
             pass
-
-    print('Accuracy : ',acc / len(y))
+        sum = sum + 1
+        print("-----------------------")
+    print(acc/sum)
